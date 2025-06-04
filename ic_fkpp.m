ea79f11a6f58a90e19111180145e35692a062543
fdarango <fdarangoo@ut.edu.co>
2025-06-03 20:53:19 -0500

    Update ic_fkpp.m

--- ic_fkpp.m
+++ ic_fkpp.m
@@ -1,20 +1,15 @@
 function x0 = ic_fkpp(Nm, d)
-    % intervalo para integrar numericamente
     Nq = 400;
     x = linspace(0, d, Nq);
-    
-    % condicion inicial
-    u0 = sin(pi * x / d);
+    u0 = sin(pi*x/d);
 
-    % funciones base
     phi = cell(Nm,1);
     for n = 1:Nm
         phi{n} = sqrt(2/d) * sin(n * pi * x / d);
     end
 
-    % calculo condiciones iniciales
     x0 = zeros(Nm,1);
     for n = 1:Nm
-        x0(n) = sum(u0 .* phi{n});
-    end
+        numerator = trapz(x, u0 .* phi{n});
+        x0(n) = numerator;
 end
